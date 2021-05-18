9218479ba435868189e43374dbc3ff3b136944bc
varun8060 <84367043+varun8060@users.noreply.github.com>
2021-05-18 10:01:24 +0530

    Area of circle code

---
+++ mycaptain_1.py
@@ -0,0 +1,5 @@
+#program to find area of circle in Python 
+PI = 3.14
+r = float(input("Input the radius of the circle: "))
+area = PI * r * r
+print("The area of circle with radius",r, "is:" "%.3f" %area)
